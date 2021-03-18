f4a3e89c14d369d3f4c97d16bf932a72ce7faf1e
yasin610 <65125483+yasinctg6@users.noreply.github.com>
2021-03-18 15:30:30 +0600

    Update create.blade.php

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -24,7 +24,7 @@
             @endif
 			<div class="form-group">
 				<label for="name">Name</label>
-				<input type="text" id="name" class="form-control" value="{{ isset($category)? $category->name:'' }}">
+				<input type="text" id="name" name="name" class="form-control" value="{{ isset($category)? $category->name:'' }}">
 			</div>
 			<div class="form-group">
 				<button class="btn btn-success">{{isset($category) ? 'Update Category' : 'Add Category'}}</button>
